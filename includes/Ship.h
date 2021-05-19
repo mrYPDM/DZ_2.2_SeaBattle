82fb017bb0999300b5674e7b1db29dc6983dff82
mrYPDM <mrypdm@gmail.com>
2021-05-19 09:31:14 +0300

    ship data is const now

--- includes/Ship.h
+++ includes/Ship.h
@@ -2,11 +2,11 @@
 
 struct Ship
 {
-	size_t X;
-	size_t Y;
-	short Length;
+	const size_t X;
+	const size_t Y;
+	const short Length;
 	short HP;
-	short Direction;
+	const short Direction;
 
 	Ship(size_t x, size_t y, short len, short dir) : X(x), Y(y), Length(len), Direction(dir), HP(len)
 	{
